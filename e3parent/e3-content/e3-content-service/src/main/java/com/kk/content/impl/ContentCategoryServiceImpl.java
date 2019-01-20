bcdbd56fb16556708d7bed6d3a4e7198f2ec02a1
kk <2437752322@qq.com>
2019-01-20 23:00:06 +0800

    内容模块添加

---
+++ e3parent/e3-content/e3-content-service/src/main/java/com/kk/content/impl/ContentCategoryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.kk.content.impl;
+
+import com.kk.content.*;
+import com.kk.pojo.EasyUITreeNode;
+
+import java.util.List;
+
+public class ContentCategoryServiceImpl implements ContentCategoryService {
+
+    public List<EasyUITreeNode> getContentCategory(Long parentId) {
+        return null;
+    }
+}
